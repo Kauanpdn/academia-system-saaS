2af4f7b57015676ad7ab36150a321f67e81b1304
OkauanDev <teko4278@gmail.com>
2026-06-17 17:56:39 -0300

    começando os controllesr

--- backend/src/main/java/academia/backend/controller/AcademyController.java
+++ backend/src/main/java/academia/backend/controller/AcademyController.java
@@ -1,7 +1,46 @@
 package academia.backend.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import academia.backend.entity.Academy;
+import academia.backend.service.AcademyService;
+
+@RestController
+@RequestMapping("/academies")
 public class AcademyController {
 
+    
+    @Autowired
+    private AcademyService academyService;
+
+    @GetMapping
+    public List<Academy> findAll() {
+        return academyService.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Academy findById(@PathVariable Integer id){
+        return academyService.findById(id);
+    }
+
+    @PostMapping()
+    public Academy save(@RequestBody Academy academy){
+        return academyService.save(academy);
+    }
+
+    @PutMapping("/{id}")
+    public Academy upadate(@PathVariable Integer id, @RequestBody Academy academy){
+        return academyService.update(id, academy);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        academyService.delete(id);
+    }
 
 }
---
+++ backend/src/main/java/academia/backend/controller/UserController.java
@@ -0,0 +1,15 @@
+package academia.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import academia.backend.entity.User;
+import academia.backend.service.UserService;
+
+@RestController
+@RequestMapping("/Users")
+public class UserController {
+    
+}
